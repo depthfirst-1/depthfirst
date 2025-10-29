50b88ee9c8916c065ba5978ce6d5fca4d99c86ac
depthfirst-1 <depthfirst1@gmail.com>
2025-10-29 10:38:27 -0400

    course schedule

---
+++ challenges/course_schedule.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+from typing import List
+
+def canFinish(numCourses: int, prerequisites: List[List[int]]) -> bool:
+  # Build adjacency list
+  graph = defaultdict(list)
+  for course, prerequisite in prerequisites:
+      graph[prerequisite].append(course)
+
+  # GRAY: Undiscovered, ORANGE: Discovered, GREEN: Explored
+  color = {i: 'GRAY' for i in range(numCourses)}
+
+  def dfs(node):
+      if color[node] == 'ORANGE':
+          return True  # cycle detected
+
+      if color[node] == 'GREEN':
+          return False  # already explored
+
+      color[node] = 'ORANGE'  # mark as discovered
+
+      for neighbor in graph[node]:
+          if dfs(neighbor):
+              return True
+
+      color[node] = 'GREEN'  # mark as explored
+      return False
+
+  for node in range(numCourses):
+      if color[node] == 'GRAY':
+          if dfs(node): # RUN DFS only if the state is undiscovered.
+              return False  # cycle found
+
+  return True  # no cycles detected
